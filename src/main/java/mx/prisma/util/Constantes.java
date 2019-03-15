d4e41265cb3e282ba9889413fcd1086a92f24e29
LuisGerardoJimenez <lgjc1im11@gmail.com>
2019-03-15 15:15:37 -0600

    Modificando negocio registrar proyecto

--- src/main/java/mx/prisma/util/Constantes.java
+++ src/main/java/mx/prisma/util/Constantes.java
@@ -2,7 +2,8 @@ package mx.prisma.util;
 
 public final class Constantes {
 	
-	//NUMEROS CONSTANTES
+	//NUMEROS CONSTANTES ENTEROS
+	public static final Integer NUMERO_CERO = 0;
 	public static final Integer NUMERO_UNO_NEGATIVO = -1;
 	public static final Integer NUMERO_UNO = 1;
 	public static final Integer NUMERO_DOS = 2;
@@ -14,11 +15,18 @@ public final class Constantes {
 	public static final Integer NUMERO_OCHO = 8;
 	public static final Integer NUMERO_NUEVE = 9;
 	public static final Integer NUMERO_DIEZ = 10;
+	public static final Integer NUMERO_ONCE = 11;
+	public static final Integer NUMERO_DOCE = 12;
 	public static final Integer NUMERO_DIECIOCHO = 18;
 	public static final Integer NUMERO_VEINTE = 20;
 	public static final Integer NUMERO_TREINTA = 30;
 	public static final Integer NUMERO_CUARENTA_Y_CINCO = 45;
 	public static final Integer NUMERO_CINCUENTA = 50;
+	public static final Integer NUMERO_CIEN = 100;
+	public static final Integer NUMERO_NOVECIENTOS_NOVENTA_Y_NUEVE = 999;
+	
+	//NUMEROS CONSTANTES DOUBLES
+	public static final Double NUMERO_MIL_MILLONES = 1000000000.00;
 	
 	//ERRORES
 	public static final String NUMERO_UNO_NEGATIVO_STRING = "-1";
